fdb7372ab07bfbb1c05cd9d93f4f6ef094d05933
Will Alexander <will@beanstockmedia.com>
2014-02-24 14:12:36 -0500

    add auto completer

--- zsh/.config/zsh/profile.d/completion.zsh
+++ zsh/.config/zsh/profile.d/completion.zsh
@@ -1,4 +1,5 @@
 fpath=($HOME/.config/zsh/functions /usr/local/share/zsh-completions $fpath)
+. /usr/local/bin/aws_zsh_completer.sh
 autoload -U compinit
 compinit
 zstyle ':completion:*' menu select=2
