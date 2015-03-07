2d86437d69c23f4a1a242190c91ac8861d42134c
Will Alexander <will@beanstockmedia.com>
2015-03-07 18:27:28 -0500

    key timeout

--- zsh/.config/zsh/profile.d/powerline.zsh
+++ zsh/.config/zsh/profile.d/powerline.zsh
@@ -1,5 +1,5 @@
 powerline_dir=$(pip show powerline-status | grep Location | cut -d":" -f2 | gsed 's/\s//g' )/powerline/bindings
-
+powerline-daemon -q
 powerline_init_file=$powerline_dir/zsh/powerline.zsh
 
 if [ -f $powerline_init_file ]; then
